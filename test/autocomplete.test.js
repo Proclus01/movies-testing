01bd0764598dbc45d9fd294d89e760131141f676
Neven <95269427+Proclus01@users.noreply.github.com>
2022-08-22 22:10:22 -0700

    wire up chai to test.html and use chai bdd expect in order to test whether dropdown menu is-active flag is not activated

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -12,4 +12,10 @@ it('Shows an autocomplete', () => {
             return movie.Title;
         }
     });
+
+    // Check if dropdown is-active
+    const dropdown = document.querySelector('.dropdown');
+
+    // Ensure that dropdown is-active is NOT added to the dropdown class
+    expect(dropdown.className).not.to.include('is-active');
 });
